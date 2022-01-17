4e0e44ade720bd64a850a424799e4a163dc2373a
Thiago <thiago.ralves02@gmail.com>
2022-01-16 23:43:51 -0300

    feat: context added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Pages/Home'
 import './App.css'
+import CtxApp from './context/CtxApp';
 
 export default function App() {
     return (
         <>
-        <Router>
-            <Navbar/>
-            <Switch>
-                <Route path='/' exact={true} component ={Home}/>
-            </Switch>
-        </Router>
+        <CtxApp>
+            <Router>
+                <Navbar/>
+                <Switch>
+                    <Route path='/' exact={true} component ={Home}/>
+                </Switch>
+            </Router>
+        </CtxApp>
         </>
     )
 }
