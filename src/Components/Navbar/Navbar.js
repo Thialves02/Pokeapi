ab9d6e11d55fa7cbc8f0e8845c8e23d4f76159d1
Thiago <thiago.ralves02@gmail.com>
2022-01-19 20:27:25 -0300

    feat: code cleaned

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,46 +1,47 @@
-import React, { useContext } from 'react'
-import './Navbar.css'
-import logo from '../../Assets/Images/logo.png'
-import { Context } from '../../context/CtxApp'
-import { Link, useHistory } from 'react-router-dom'
+import React, { useContext } from "react";
+import "./Navbar.css";
+import logo from "../../Assets/Images/logo.png";
+import { Context } from "../../context/CtxApp";
+import { Link, useHistory } from "react-router-dom";
 
 export default function Navbar() {
-    const {data,setData} = useContext(Context)
-    const history = useHistory()
+  const { setData } = useContext(Context);
+  const history = useHistory();
 
-    const search = async (e) =>{
-        e.preventDefault()
+  const search = async (e) => {
+    e.preventDefault();
 
-        const searchPokemon =  e.target.searchPokemon.value
-        if (searchPokemon === ''){
-            const response =  await fetch (
-                `https://pokeapi.co/api/v2/pokemon?limit=0&offset=0`
-            )
-            history.push('/')
-            const body = await response.json()
+    const searchPokemon = e.target.searchPokemon.value;
+    if (searchPokemon === "") {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon?limit=0&offset=0`
+      );
+      history.push("/");
+      const body = await response.json();
 
-            setData(body.results)
-            console.log(data)
-        }else{
-            const response = await fetch (
-                `https://pokeapi.co/api/v2/pokemon/${searchPokemon}`
-            )
-            history.push('/')
-            const body = await response.json()
-            setData(body)
-            console.log(body)
-        }
-        
+      setData(body.results);
+    } else {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${searchPokemon}`
+      );
+      history.push("/");
+      const body = await response.json();
+      setData(body);
     }
-    return (
-        <div className='navbar-container'>
-            <Link to='/'><img src={logo}/></Link>
-            <div className='form'>
-            <form onSubmit={search}>
-                <input type='text' placeholder='Pesquisar' name='searchPokemon'/>
-            </form>
-            </div>
-            <Link to='/time'><p>Meu time</p></Link>
-        </div>
-    )
+  };
+  return (
+    <div className="navbar-container">
+      <Link to="/">
+        <img src={logo} />
+      </Link>
+      <div className="form">
+        <form onSubmit={search}>
+          <input type="text" placeholder="Pesquisar" name="searchPokemon" />
+        </form>
+      </div>
+      <Link to="/time">
+        <p>Meu time</p>
+      </Link>
+    </div>
+  );
 }
