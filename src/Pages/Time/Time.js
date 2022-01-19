3b20eecd28bde4dd11847157d6c72cc8332e398b
Thiago <thiago.ralves02@gmail.com>
2022-01-18 23:06:36 -0300

    feat: page Time created

--- src/Pages/Time/Time.js
+++ src/Pages/Time/Time.js
@@ -1,23 +1,50 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { Context } from '../../context/CtxApp'
+import React, { useContext } from "react";
+import "./Time.css";
+import treinador from "../../Assets/Images/treinador.png";
+import { Context } from "../../context/CtxApp";
 
 export default function Time() {
-    const {favName,setFavName} = useContext(Context)
-    const [info,setInfo] = useState()
-    /* const [time,setTime] = useState([]) */
+  const con = JSON.parse(localStorage.getItem("favorites"));
+  console.log(con);
+  const { BACKGROUND_COLORS } = useContext(Context);
 
-    const con = JSON.parse(localStorage.getItem('favorites'))
-    
-    const values = con.filter(function (a) {
-        return !this[JSON.stringify(a)] && (this[JSON.stringify(a)] = true);
-    }, Object.create(null))
-    
-    
-    /* setTime(values) */
-    console.log(values)
-    /* console.log(time) */
-    return (
-        <>
-        </>
-    )
+  return (
+    <>
+      <h1 className="time-title">Seu time</h1>
+      <div className="container-time">
+        {con === null && (
+          <>
+            <div>
+              Clique na estrela do pokemon que você deseja adicionar ao seu time
+            </div>
+          </>
+        )}
+        {con != undefined && (
+          <>
+            <img src={treinador} alt="Treinador" />
+            <div className="pokemons">
+              {con.length === 0 && (
+                <div>
+                  Você retirou todos seus pokemons e agora está sem time
+                </div>
+              )}
+              {con.length !== 0 &&
+                con.map((favorite, index) => (
+                  <div className="pokemon">
+                    <img
+                      src={favorite.sprites.front_default}
+                      style={{
+                        backgroundColor: `#${
+                          BACKGROUND_COLORS[favorite.types[0].type.name]
+                        }`,
+                      }}
+                    />
+                  </div>
+                ))}
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
 }
