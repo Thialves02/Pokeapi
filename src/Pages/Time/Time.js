0d582cb20ddcb36e81e8d35dd81fdb9c4a60a8c1
Thiago <thiago.ralves02@gmail.com>
2022-01-31 14:10:13 -0300

    feat: star of favorites on team

--- src/Pages/Time/Time.js
+++ src/Pages/Time/Time.js
@@ -2,6 +2,8 @@ import React, { useContext, useEffect, useState } from "react";
 import "./Time.css";
 import treinador from "../../Assets/Images/treinador.png";
 import { Context } from "../../context/CtxApp";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 export default function Time() {
   const [favorites, setFavorites] = useState();
@@ -22,23 +24,29 @@ export default function Time() {
       <div className="container-time">
         {con === null && (
           <>
-            <div>
+            <div className="infos-time">
               Clique na estrela do pokemon que você deseja adicionar ao seu time
             </div>
           </>
         )}
         {con != undefined && (
           <>
-            <img src={treinador} alt="Treinador" />
+            <img src={treinador} alt="Treinador" className="treinador" />
             <div className="pokemons">
               {con.length === 0 && (
-                <div>
+                <div className="infos-time">
                   Você retirou todos seus pokemons e agora está sem time
                 </div>
               )}
               {con.length !== 0 &&
                 con.map((favorite, index) => (
-                  <div className="pokemon" onClick={() => removePokemon(index)}>
+                  <div className="pokemon">
+                    <div className="fav">
+                      <FontAwesomeIcon
+                        icon={faStar}
+                        onClick={() => removePokemon(index)}
+                      />
+                    </div>
                     <img
                       src={favorite.sprites.front_default}
                       style={{
